925e3720dded939c0e205f94f590dd350799c111
zbrookle <zachb1996@yahoo.com>
2021-01-23 23:22:38 -0500

    ENH: Add scheduled chain

--- workers/project/tasks.py
+++ workers/project/tasks.py
@@ -1,24 +1,44 @@
-from .application import app
-from celery import chain
-from .web_search import get_search_results, parse_results
+from project.application import app
+from project.web_search import get_search_results, parse_results
+from typing import Callable, List
+import random
 
 terms = ["test term", "test term2", "test term3", "test term4"]
 
 
-@app.task
-def retrieve_search_page(term: str):
+def fail_chance_decorator(function: Callable):
+    num = random.randint(1, 10)
+    if num <= 2:
+
+        def retfunc(*args, **kwargs):
+            raise Exception("Failure!")
+
+        return retfunc
+    return function
+
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    for term in terms:
+        sender.add_periodic_task(10.0, chained_process.s(term))
+
+
+@app.task(bind=True, name='chained_process', track_started=True)
+def chained_process(self, term: str):
+    chain_sig = retrieve_search_page.s(term) | parse.s() | count_results.s()
+    chain_sig()
+
+
+@app.task(bind=True)
+def retrieve_search_page(self, term: str):
     return get_search_results(f"http://www.google.com/search?q={term}")
 
 
-@app.task
-def parse(result_html: str):
+@app.task(bind=True)
+def parse(self, result_html: str):
     return parse_results(result_html)
 
 
-@app.task
-def count_results(result_urls: list):
+@app.task(bind=True)
+def count_results(self, result_urls: List[str]):
     return len(result_urls)
-
-
-for term in terms:
-    chain(retrieve_search_page.s(term) | parse.s()).delay()
